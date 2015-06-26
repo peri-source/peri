small fixes, including factor of 2 in LL; acknowledge psf influence on tile size

cbamf/states.py
@@ -110,7 +110,7 @@ class ConfocalImagePython(State):
 
     def _build_internal_variables(self):
         self.model_image = np.zeros_like(self.image)
-        self._loglikelihood_field = -self.image_mask*self.image**2 / self.sigma**2
+        self._loglikelihood_field = -self.image_mask*self.image**2 / (2*self.sigma**2)
         self._loglikelihood = self._loglikelihood_field.sum()
         self._logprior = 0
 
@@ -126,6 +126,7 @@ class ConfocalImagePython(State):
         self._update_tile(*self._tile_global())
 
     def _tile_from_particle_change(self, p0, r0, p1, r1):
+        # TODO - add the psf into the calculation of the tile size
         zsc = np.array([1.0/self.zscale, 1, 1])
         r0, r1 = zsc*r0, zsc*r1
         pl = np.round(np.vstack(
@@ -160,7 +161,7 @@ class ConfocalImagePython(State):
         replacement = self.psf.execute(replacement) + self.offset
 
         self.model_image[islice] = replacement[ioslice]
-        self._loglikelihood_field[islice] = -self.image_mask[islice]*(replacement[ioslice] - self.image[islice])**2 / self.sigma**2
+        self._loglikelihood_field[islice] = -self.image_mask[islice]*(replacement[ioslice] - self.image[islice])**2 / (2*self.sigma**2)
 
         newll = self._loglikelihood_field[islice].sum()
         self._loglikelihood += newll - oldll

scripts/block-shuffling.py
@@ -30,30 +30,16 @@ itrue += np.random.normal(0.0, sigma, size=itrue.shape)
 strue = s.state.copy()
 s.set_image(itrue)
 
-#raise IOError
-if True:
-    h = []
-    for i in xrange(sweeps):
-        print '{:=^79}'.format(' Sweep '+str(i)+' ')
+blocks = s.explode(s.block_all())
+h = []
+for i in xrange(sweeps):
+    print '{:=^79}'.format(' Sweep '+str(i)+' ')
 
-        runner.sample_particles(s)
-        runner.sample_block(s, 'psf', explode=False)
-        runner.sample_block(s, 'ilm', explode=False)
-        runner.sample_block(s, 'off', explode=True)
-        #run.sample_block(s, 'zscale', explode=True)
+    np.random.shuffle(blocks)
+    for block in blocks:
+        runner.sample_state(s, [block], stepout=0.1)
 
-        if i >= burn:
-            h.append(s.state.copy())
+    if i >= burn:
+        h.append(s.state.copy())
 
-    h = np.array(h)
-
-mu = h.mean(axis=0)
-std = h.std(axis=0)
-pl.figure(figsize=(20,4))
-pl.errorbar(xrange(len(mu)), (mu-strue), yerr=5*std/np.sqrt(samples),
-        fmt='.', lw=0.15, alpha=0.5)
-pl.vlines([0,3*s.N-0.5, 4*s.N-0.5], -1, 1, linestyle='dashed', lw=4, alpha=0.5)
-pl.hlines(0, 0, len(mu), linestyle='dashed', lw=5, alpha=0.5)
-pl.xlim(0, len(mu))
-pl.ylim(-0.02, 0.02)
-pl.show()
+h = np.array(h)

scripts/single-particle-hessian.py
@@ -10,7 +10,7 @@ from cbamf.viz import plots
 PAD = 16
 sigma = 0.05
 
-imsize = (64,64,64)
+imsize = (64,)*3
 blank = np.zeros(imsize, dtype='float')
 xstart, rstart = np.array(imsize).reshape(-1,3)/2.0, np.array([5.0])
 
@@ -24,5 +24,8 @@ itrue += np.random.normal(0.0, sigma, size=itrue.shape)
 strue = s.state.copy()
 s.set_image(itrue)
 
-hess = -s.hessloglikelihood()
-fischer = np.linalg.inv(hess)
+hess = s.hessloglikelihood()
+print "Hessian:", np.diag(hess)
+
+#h, ll = runner.do_samples(s, 30, 5)
+#plots.summary_plot(s, h, truestate=strue)