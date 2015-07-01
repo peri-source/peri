bc7311dbdd0626e44869015eb1f56406ce39daa1
Matt Bierbaum <matt.bierbaum@gmail.com>
2015-07-01 16:53:09 -0400

    adding particles iteratively seems to be working well with test data

--- scripts/frozen-particles.py
+++ scripts/frozen-particles.py
@@ -13,8 +13,11 @@ def pad_fake_particles(pos, rad, nfake):
     orad = np.hstack([rad, rad[0]*np.ones(nfake)])
     return opos, orad
 
+def zero_particles(n):
+    return np.zeros((n,3)), np.ones(n), np.zeros(n)
+
 #raise IOError
-ORDER = (4,4,3)
+ORDER = (3,3,2)
 sweeps = 30
 samples = 20
 burn = sweeps - samples
@@ -34,7 +37,8 @@ itrue = np.pad(itrue, PAD, mode='constant', constant_values=-10)
 xstart += PAD
 rstart = RAD*np.ones(xstart.shape[0])
 initializers.remove_overlaps(xstart, rstart, zscale=zscale)
-nfake = 20
+nfake = 50
+#xstart, rstart, tstart = zero_particles(nfake)
 xstart, rstart = pad_fake_particles(xstart, rstart, nfake)
 
 imsize = itrue.shape
@@ -43,9 +47,9 @@ psf = psfs.AnisotropicGaussian(PSF, shape=imsize)
 ilm = ilms.Polynomial3D(order=ORDER, shape=imsize)
 ilm.from_data(itrue, mask=itrue > -10)
 
-diff = (ilm.get_field() - itrue)[itrue > -10].max()
+diff = (ilm.get_field() - itrue)
 params = ilm.get_params()
-params[0] += diff/2
+params[0] += diff[itrue > -10].max() / 2
 #params = np.load("/media/scratch/bamf/frozen-particles/ilm.npy")
 ilm.update(params)
 
@@ -55,39 +59,52 @@ s = states.ConfocalImagePython(itrue, obj=obj, psf=psf, ilm=ilm,
 
 import scipy.ndimage as nd
 
-def sample_add(s, rad=5):
-    diff = s.get_model_image() - s.image
+def sample_add(s, rad=5, tries=5):
+    diff = (s.get_model_image() - s.image).copy()
 
-    smoothdiff = nd.gaussian_filter(diff, 1)
-    eq = smoothdiff == smoothdiff.max()
+    smoothdiff = nd.gaussian_filter(diff, rad/2.0)
+    maxfilter = nd.maximum_filter(smoothdiff, size=rad)
+    eq = smoothdiff == maxfilter
     lbl = nd.label(eq)[0]
-    pos = np.array(nd.center_of_mass(eq, lbl, np.unique(lbl)))[1:]
+    pos = np.array(nd.center_of_mass(eq, lbl, np.unique(lbl)))[1:].astype('int')
+    ind = np.arange(len(pos))
 
-    #np.unravel_index
-    n = s.obj.typ.argmin()
-    bp = s.block_particle_pos(n)
-    br = s.block_particle_rad(n)
-    bt = s.block_particle_typ(n)
-    s.update(bp, pos)
-    s.update(br, np.array([rad]))
-    s.update(bt, np.array([1]))
+    val = [maxfilter[tuple(pos[i])] for i in ind]
+    vals = sorted(zip(val, ind))
 
-    bl = s.blocks_particle(n)
-    runner.sample_state(s, bl, stepout=1, N=3)
+    for _, i in vals[-tries:][::-1]:
+        diff = (s.get_model_image() - s.image)
 
-    diff2 = s.get_model_image() - s.image
+        p = pos[i].reshape(-1,3)
+        n = s.obj.typ.argmin()
 
-    if np.log(np.random.rand()) > (diff2**2).sum() - (diff**2).sum():
-        neighs = s.nbl.neighs[n].keys()
-        print "Added, modifying neighbors"
-        for neigh in neighs:
-            print neigh
-            bl = s.blocks_particle(neigh)
-            runner.sample_state(s, bl, stepout=0.3)
-        return True
-    else:
-        s.update(bt, np.array([0]))
-    return False
+        bp = s.block_particle_pos(n)
+        br = s.block_particle_rad(n)
+        bt = s.block_particle_typ(n)
+
+        s.update(bp, p)
+        s.update(br, np.array([rad]))
+        s.update(bt, np.array([1]))
+
+        #return n, bp, br, bt
+        bl = s.blocks_particle(n)[:-1]
+        runner.sample_state(s, bl, stepout=1, N=1)
+
+        diff2 = (s.get_model_image() - s.image)
+
+        print p, (diff**2).sum(), (diff2**2).sum()
+        if np.log(np.random.rand()) > (diff2**2).sum() - (diff**2).sum():
+            #neighs = s.nbl.neighs[n].keys()
+            #print "Added, modifying neighbors"
+            #for neigh in neighs:
+            #    print neigh
+            #    bl = s.blocks_particle(neigh)
+            #    runner.sample_state(s, bl, stepout=0.3)
+            #return True
+            pass
+        else:
+            s.update(bt, np.array([0]))
+    #return False
 
 def sample_remove(s, rad=5):
     diff = s.get_model_image() - s.image
@@ -114,14 +131,6 @@ def sample_remove(s, rad=5):
     else:
         s.update(bt, np.array([1]))
     return False
-
-def sample_good_particles(s, N=1):
-    m = np.arange(s.N)[s.obj.typ == 1]
-    for j in xrange(N):
-        for i in m:
-            print i
-            b = s.blocks_particle(i)
-            runner.sample_state(s, b, stepout=1)
 
 def iterate(s, n=10):
     for i in xrange(n):
