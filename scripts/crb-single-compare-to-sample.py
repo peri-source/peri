7919620b189c53d19ed29f46e0f9282b933914c4
Matt Bierbaum <matt.bierbaum@gmail.com>
2015-07-09 15:17:31 -0400

    sampling the sampling error to compare crb

--- scripts/crb-single-compare-to-sample.py
+++ scripts/crb-single-compare-to-sample.py
@@ -4,7 +4,9 @@ import pylab as pl
 from cbamf import runner
 from cbamf.test import init
 
-samples = 100
+samples = 10
+sweeps = 30
+
 
 # the final answers, crbs and stds
 crbs = []
@@ -30,26 +32,38 @@ for rad in rads:
 
     hist = []
     for i in xrange(samples):
-        hist.append(runner.sample_particles(s, stepout=0.1))
+        print "radius", rad, "sample", i
+        s.model_to_true_image()
+
+        thist = []
+        for j in xrange(sweeps):
+            thist.append(runner.sample_particles(s, stepout=0.1))
+        hist.append(thist)
     hist = np.array(hist)
     hists.append(hist)
-    stds.append(hist.std(axis=0)) 
+
+colors = np.array([
+    (15,77,192,255), (250,196,4,255), (50,152,51,255), (241,57,50,255)
+]) / 255.0
+
+hists = np.array(hists)
 
 crbs = 1.0 / np.sqrt(np.array(crbs))
-stds = np.array(stds)
+stdmu = hists.std(axis=-2).mean(axis=-2)
+stderr = hists.std(axis=-2).std(axis=-2)
 
 pl.figure()
-pl.loglog(rads, crbs[:,0], 'o-', lw=1, label='crb pos-z')
-pl.loglog(rads, crbs[:,1], 'o-', lw=1, label='crb pos-y')
-pl.loglog(rads, crbs[:,2], 'o-', lw=1, label='crb pos-x')
-pl.loglog(rads, crbs[:,3], 'o-', lw=1, label='crb rad')
+pl.loglog(rads, crbs[:,0], 'o-', lw=1.5, c=colors[0], alpha=0.6, label='CRB pos-z')
+pl.loglog(rads, crbs[:,1], 'o-', lw=1.5, c=colors[1], alpha=0.6, label='CRB pos-y')
+pl.loglog(rads, crbs[:,2], 'o-', lw=1.5, c=colors[2], alpha=0.6, label='CRB pos-x')
+pl.loglog(rads, crbs[:,3], 'o-', lw=1.5, c=colors[3], alpha=0.6, label='CRB rad')
 
-pl.loglog(rads, stds[:,0], 'o-', lw=1, label='std pos-z')
-pl.loglog(rads, stds[:,1], 'o-', lw=1, label='std pos-y')
-pl.loglog(rads, stds[:,2], 'o-', lw=1, label='std pos-x')
-pl.loglog(rads, stds[:,3], 'o-', lw=1, label='std rad')
+pl.errorbar(rads, stdmu[:,0], yerr=stderr[:,0], fmt='o', lw=1.5, c=colors[0], label='STD pos-z')
+pl.errorbar(rads, stdmu[:,1], yerr=stderr[:,1], fmt='o', lw=1.5, c=colors[1], label='STD pos-y')
+pl.errorbar(rads, stdmu[:,2], yerr=stderr[:,2], fmt='o', lw=1.5, c=colors[2], label='STD pos-x')
+pl.errorbar(rads, stdmu[:,3], yerr=stderr[:,3], fmt='o', lw=1.5, c=colors[3], label='STD rad')
 
-pl.legend(loc='upper right')
+pl.legend(loc='upper right', numpoints=1, ncol=2, prop={'size': 20})
 pl.xlabel("Radius")
 pl.ylabel("Variance")
 pl.show()
