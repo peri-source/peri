4116d54b784917aed78ee018334e2eea00dc3cc3
Matt Bierbaum <matt.bierbaum@gmail.com>
2015-07-09 16:48:13 -0400

    smoother crb line, and not points on crb

--- scripts/crb-single-compare-to-sample.py
+++ scripts/crb-single-compare-to-sample.py
@@ -14,9 +14,8 @@ stds = []
 hists = []
 
 # options for the size distribution
-rads = np.arange(1, 10, 1./5)
-rads = np.linspace(1, 10, 39)
-rads = np.logspace(0, 1, 10)
+rads = np.logspace(0, 1, 50)
+rads2 = np.logspace(0, 1, 10)
 
 for rad in rads:
     print "Radius", rad
@@ -29,6 +28,13 @@ for rad in rads:
     for block in blocks:
         crb.append( s.fisher_information([block])[0,0] )
     crbs.append(crb)
+
+for rad in rads2:
+    print "Radius", rad
+
+    # create a single particle state and get pos/rad blocks
+    s = init.create_single_particle_state(imsize=64, radius=rad, sigma=0.05)
+    blocks = s.blocks_particle(0)
 
     hist = []
     for i in xrange(samples):
@@ -53,15 +59,15 @@ stdmu = hists.std(axis=-2).mean(axis=-2)
 stderr = hists.std(axis=-2).std(axis=-2)
 
 pl.figure()
-pl.loglog(rads, crbs[:,0], 'o-', lw=1.5, c=colors[0], alpha=0.6, label='CRB pos-z')
-pl.loglog(rads, crbs[:,1], 'o-', lw=1.5, c=colors[1], alpha=0.6, label='CRB pos-y')
-pl.loglog(rads, crbs[:,2], 'o-', lw=1.5, c=colors[2], alpha=0.6, label='CRB pos-x')
-pl.loglog(rads, crbs[:,3], 'o-', lw=1.5, c=colors[3], alpha=0.6, label='CRB rad')
+pl.loglog(rads, crbs[:,0], '-', lw=1.5, c=colors[0], alpha=0.6, label='CRB pos-z')
+pl.loglog(rads, crbs[:,1], '-', lw=1.5, c=colors[1], alpha=0.6, label='CRB pos-y')
+pl.loglog(rads, crbs[:,2], '-', lw=1.5, c=colors[2], alpha=0.6, label='CRB pos-x')
+pl.loglog(rads, crbs[:,3], '-', lw=1.5, c=colors[3], alpha=0.6, label='CRB rad')
 
-pl.errorbar(rads, stdmu[:,0], yerr=stderr[:,0], fmt='o', lw=1.5, c=colors[0], label='STD pos-z')
-pl.errorbar(rads, stdmu[:,1], yerr=stderr[:,1], fmt='o', lw=1.5, c=colors[1], label='STD pos-y')
-pl.errorbar(rads, stdmu[:,2], yerr=stderr[:,2], fmt='o', lw=1.5, c=colors[2], label='STD pos-x')
-pl.errorbar(rads, stdmu[:,3], yerr=stderr[:,3], fmt='o', lw=1.5, c=colors[3], label='STD rad')
+pl.errorbar(rads2, stdmu[:,0], yerr=stderr[:,0], fmt='o', lw=1.5, c=colors[0], label='STD pos-z')
+pl.errorbar(rads2, stdmu[:,1], yerr=stderr[:,1], fmt='o', lw=1.5, c=colors[1], label='STD pos-y')
+pl.errorbar(rads2, stdmu[:,2], yerr=stderr[:,2], fmt='o', lw=1.5, c=colors[2], label='STD pos-x')
+pl.errorbar(rads2, stdmu[:,3], yerr=stderr[:,3], fmt='o', lw=1.5, c=colors[3], label='STD rad')
 
 pl.legend(loc='upper right', numpoints=1, ncol=2, prop={'size': 20})
 pl.xlabel("Radius")
