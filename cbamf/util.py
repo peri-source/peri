bb1106d61ecdeb2a973d68cb0f2ef01d88bee720
Matt Bierbaum <matt.bierbaum@gmail.com>
2015-07-10 15:54:29 -0400

    updating progress bar for more styles

--- cbamf/util.py
+++ cbamf/util.py
@@ -52,41 +52,121 @@ def cdd(d, k):
 # Progress bar
 #=============================================================================
 class ProgressBar(object):
-    def __init__(self, num, label='Progress', value=0, dobar=True, symbol='='):
+    def __init__(self, num, label='Progress', value=0, screen=79,
+            bar=True, bar_symbol='=', bar_caps='[]', bar_decimals=2):
+        """
+        ProgressBar class which creates a dynamic ASCII progress bar of two
+        different varieties:
+            1) A bar chart that looks like the following:
+                Progress [================      ]  63.00%
+
+            2) A simple number completed look:
+                Progress :   17 / 289
+
+        Parameters:
+        -----------
+        num : integer
+            The number of tasks that need to be completed
+
+        label : string [default: 'Progress']
+            The label for this particular progress indicator, 
+
+        value : integer [default: 0]
+            Starting value
+
+        screen : integer [default: 79]
+            Size the screen to use for the progress bar
+
+        bar : boolean [default: True]
+            Whether or not to display the bar chart
+
+        bar_symbol : char [default: '=']
+            The character to use to fill in the bar chart
+
+        bar_caps : string [default: '[]']
+            Characters to use as the end caps of the.  The string will be split in
+            half and each half put on a side of the chart
+
+        bar_decimals: integer [default: 2]
+            Number of decimal places to include in the percentage
+        """
         self.num = num
         self.value = value
         self.percent = 0
 
         self.label = label
-        self.dobar = dobar
-        self.symbol = symbol
+        self.bar = bar
+        self.bar_symbol = bar_symbol
+        self.bar_caps = bar_caps
+        self.bar_decimals = bar_decimals
+        self.screen = screen
 
-        # the space available for the progress bar is
-        # 79 (size of screen) - (label) - 3 (number) - 2 ([]) - 2 (space)
-        self._bar_size = 79 - len(self.label) - 3 - 2 - 2
-        self._formatstr = '{} [{: <'+str(self._bar_size)+'}] {: >3}\r'
+        if len(self.bar_caps) % 2 != 0:
+            raise AttributeError("End caps must be even number of symbols")
 
-        self._calc_precent()
-        self.draw()
+        if self.bar:
+            # 3 digit percent + decimal places + '.'
+            self._num_size = 3 + self.bar_decimals + 1
 
-    def _calc_precent(self):
-        self.percent = int(100 * self.value / self.num)
+            # end caps size calculation
+            self._cap_len = len(self.bar_caps)/2
+            self._cap_l = self.bar_caps[:self._cap_len]
+            self._cap_r = self.bar_caps[self._cap_len:]
 
-    def draw(self):
-        bar = np.round(self.percent / 100. * self._bar_size)
-        print self._formatstr.format(self.label, '='*bar, self.percent),
+            # the space available for the progress bar is
+            # 79 (screen) - (label) - (number) - 2 ([]) - 2 (space) - 1 (%)
+            self._bar_size = (
+                    self.screen - len(self.label) - self._num_size -
+                    len(self.bar_caps) - 2 - 1
+                )
+
+            self.pdict = {
+                "label": self.label,
+                "bars": '',
+                "barsize": self._bar_size,
+                "numsize": self._num_size,
+                "decs": self.bar_decimals,
+                "capl": self._cap_l,
+                "capr": self._cap_r,
+                "percent": '',
+            }
+            self._formatstr = '\r{label} {capl}{bars:<{barsize}}{capr} {percent:>{numsize}.{decs}f}%'
+        else:
+            self._digits = str(int(np.ceil(np.log10(self.num))))
+            self._formatstr = '\r{label} : {value:>{_digits}} / {num:>{_digits}}'
+
+        self.update()
+
+    def _draw(self):
+        """ Interal draw method, simply prints to screen """
+        if self.bar:
+            self.pdict.update({"percent": self.percent, "bars": self.bar_symbol*self.bars})
+            print self._formatstr.format(**self.pdict),
+        else:
+            print self._formatstr.format(**self.__dict__),
         sys.stdout.flush()
 
     def update(self, value=0):
-        curr_percent = self.percent
-        self.value = value
-        self._calc_precent()
+        """
+        Update the value of the progress and update progress bar.
 
-        if curr_percent != self.percent:
-            self.draw()
+        Parameters:
+        -----------
+        value : integer
+            The current iteration of the progress
+        """
+        self.value = value
+        self.percent = 100.0 * self.value / self.num
+
+        if self.bar:
+            self.bars = int(np.round(self.percent / 100. * self._bar_size))
+        self._draw()
+
+        if self.value == self.num:
+            self.end()
 
     def end(self):
-        print '\r\n'
+        print '\r{lett:>{screen}}'.format(**{'lett':'', 'screen': self.screen})
 
 #=============================================================================
 # debugging / python interpreter / logging
