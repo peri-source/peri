484664f3f553cac6f0f9e535817bc710b631a671
Matt Bierbaum <matt.bierbaum@gmail.com>
2015-08-30 16:47:35 -0400

    g(r) calculation

--- cbamf/test/analyze.py
+++ cbamf/test/analyze.py
@@ -28,3 +28,17 @@ def gofr_surfaces(pos, rad, zscale):
         seps.extend(diff)
     return np.array(seps)
 
+def gofr_full(pos, rad, zscale, resolution=3e-2, rmax=10, method=gofr,
+        mask_start=None):
+    d = 2*rad.mean()
+
+    o = method(pos, rad, zscale)
+    y,x = np.histogram(o/d, bins=np.linspace(0, rmax, d*rmax/resolution))
+    x = (x[1:] + x[:-1])/2
+
+    if mask_start is not None:
+        mask = x > mask_start
+        x = x[mask]
+        y = y[mask]
+
+    return x, y/(4*np.pi*x**2*resolution)
