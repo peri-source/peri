feb9ea7297803422a2c1f92926059a85d3456052
Matt Bierbaum <matt.bierbaum@gmail.com>
2015-09-23 20:03:30 -0400

    added two extra subplots to ilm residual noise

--- scripts/ilm-residuals.py
+++ scripts/ilm-residuals.py
@@ -2,10 +2,12 @@ import pickle
 import numpy as np
 import scipy as sp
 
+from cbamf.viz.plots import lbl
 from cbamf.comp import ilms
 from cbamf.initializers import load_tiff, normalize
 
 import matplotlib.pyplot as pl
+from matplotlib.gridspec import GridSpec
 from mpl_toolkits.axes_grid1 import ImageGrid
 
 ilm_file = "/media/scratch/bamf/illumination_field.tif"
@@ -51,19 +53,55 @@ def plot_noise(diff):
     dat = diff.flatten()
     sig = dat.std()
 
-    y,x = np.histogram(dat, bins=np.linspace(-5*sig, 5*sig, 200), normed=True)
+    fig = pl.figure(figsize=(13,4))
+
+    #=====================================================
+    gs2 = GridSpec(1,2, left=0.05, bottom=0.22, right=0.45, top=0.85,
+                wspace=0.25, hspace=0.25)
+
+    ax = pl.subplot(gs2[0,0])
+    ax.imshow(diff[0], cmap=pl.cm.bone)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.grid(False, which='both', axis='both')
+    ax.set_xlabel(r"$x$")
+    ax.set_ylabel(r"$y$")
+    lbl(ax, 'A')
+
+    q = np.fft.fftn(diff)
+    q = np.fft.fftshift(np.abs(q[0])**0.1)
+
+    ax = pl.subplot(gs2[0,1])
+    ax.imshow(q, cmap=pl.cm.bone)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.grid(False, which='both', axis='both')
+    ax.set_xlabel(r"$q_x$")
+    ax.set_ylabel(r"$q_y$")
+    lbl(ax, 'B')
+
+    #=====================================================
+    gs = GridSpec(1,1, left=0.59, bottom=0.22, right=0.89, top=0.85,
+                wspace=0.35, hspace=0.35)
+
+    y,x = np.histogram(dat, bins=np.linspace(-5*sig, 5*sig, 300), normed=True)
     x = (x[1:] + x[:-1])/2
 
-    pl.plot(x, y, '-', lw=2, alpha=0.6, label='Confocal image')
-    pl.plot(x, 1/np.sqrt(2*np.pi*sig**2) * np.exp(-x**2/(2*sig**2)), 'k--', lw=1.5, label='Gaussian fit')
-    pl.semilogy()
+    ax = pl.subplot(gs[0,0])
+    ax.plot(x, y, '-', lw=2, alpha=0.6, label='Confocal image')
+    ax.plot(x, 1/np.sqrt(2*np.pi*sig**2) * np.exp(-x**2/(2*sig**2)), 'k--', lw=1.5, label='Gaussian fit')
+    ax.semilogy()
 
     ymin = y[y>0].min()
     ymax = y[y>0].max()
 
-    pl.legend(loc='best', bbox_to_anchor=(0.75, 0.25))
-    pl.xlim(-5*sig, 5*sig)
-    pl.ylim(ymin, 1.35*ymax)
-    pl.xlabel("Pixel value")
-    pl.ylabel("Probability")
-    pl.grid(False, which='minor', axis='y')
+    #ax.legend(loc='best', bbox_to_anchor=(0.75, 0.25), prop={'size':18})
+    ax.locator_params(axis='x', nbins=5)
+    ax.set_xlim(-5*sig, 5*sig)
+    ax.set_ylim(ymin, 1.35*ymax)
+    ax.set_xlabel("Pixel value")
+    ax.set_ylabel("Probability")
+    ax.grid(False, which='minor', axis='y')
+    lbl(ax, 'C')
+
+    return gs
