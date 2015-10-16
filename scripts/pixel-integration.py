5b4e9221c09671bf7ee5003b704b5ee562eae01f
Matt Bierbaum <matt.bierbaum@gmail.com>
2015-10-16 13:58:31 -0400

    good final form for pxint plot

--- scripts/pixel-integration.py
+++ scripts/pixel-integration.py
@@ -1,3 +1,4 @@
+import pickle
 import numpy as np
 import scipy as sp
 import scipy.ndimage as nd
@@ -7,6 +8,7 @@ import matplotlib.pyplot as pl
 from cbamf import const, runner
 from cbamf.test import init
 from cbamf.states import prepare_image
+from cbamf.viz.util import COLORS
 
 def set_image(state, cg, sigma):
     image = cg + np.random.randn(*cg.shape)*sigma
@@ -56,22 +58,68 @@ def pxint(factor=8, dx=np.array([0,0,0])):
     # measure the true inferred parameters
     return s, cg
 
-def doplot():
+def crb(state):
+    crb = []
+
+    blocks = state.explode(s.block_all())
+    for block in blocks:
+        tc = np.sqrt(1.0/np.abs(state.fisher_information(blocks=[block])))
+        crb.append(tc)
+
+    return np.squeeze(np.array(crb))
+
+def sample(state, im, noise, N=20):
+    values, errors = [], []
+
+    for i in xrange(N):
+        print '{:=^79}'.format(' %i ' % i)
+        set_image(state, im, noise)
+        h,l = runner.do_samples(state, 30, 15)
+
+        h = np.array(h)
+        values.append(h.mean(axis=0))
+        errors.append(h.std(axis=0))
+
+    return np.array(values), np.array(errors)
+
+def dorun():
     """
     we want to display the errors introduced by pixelation so we plot:
         * zero noise, cg image, fit
         * SNR 20, cg image, fit
         * CRB for both
     """
-    s,im = pxint()
+    s,im = pxint(factor=4)
+    goodstate = s.state.copy()
 
+    set_image(s, im, 1e-6)
+    crb0 = crb(s)
+    val0, err0 = sample(s, im, 1e-6)
+
+    set_image(s, im, 5e-2)
+    crb1 = crb(s)
+    val1, err1 = sample(s, im, 5e-2)
+
+def doplot(filename='/media/scratch/peri/pixel-integration.pkl'):
     labels = [
         'pos-z', 'pos-y', 'pos-x', 'rad',
         'psf-x', 'psf-y', 'psf-z', 'ilm',
         'off', 'rscale', 'zscale', 'sigma'
     ]
 
-    set_image(s, im, 1e-6)
-    set_image(s, im, 5e-2)
+    crb0,val0,err0,crb1,val1,err1,goodstate = pickle.load(open(filename))
 
     fig = pl.figure()
+
+    pl.plot(crb0, '-', c=COLORS[0], lw=2, label=r"$\rm{SNR} = 10^6$ CRB")
+    pl.plot(err0.mean(axis=0), 'o', c=COLORS[0], label=r"$\rm{SNR} = 10^6$ Error", ms=12)
+    pl.plot(crb1, '-', c=COLORS[1], lw=2, label=r"$\rm{SNR} = 20$ CRB")
+    pl.plot(err1.mean(axis=0), 'o', c=COLORS[1], label=r"$\rm{SNR} = 20$ Error", ms=12)
+    pl.semilogy()
+    pl.ylim(1e-9, 1e0)
+    pl.xlim(0, len(labels)-3)
+    pl.xticks(xrange(len(labels)-1), labels[:-1], rotation='vertical')
+    pl.legend(bbox_to_anchor=(1.07,1.0), prop={'size': 18}, numpoints=1)
+    pl.ylabel(r"CRB, $\bar{\sigma}$")
+    pl.grid(False, which='minor', axis='both')
+    pl.title("Pixel integration")
