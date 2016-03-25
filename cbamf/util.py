d511405e8eccd578ab51cd3452c9d1e57381e48d
Matt Bierbaum <matt.bierbaum@gmail.com>
2016-03-25 17:02:59 -0400

    reflect overhang function in tiles

--- cbamf/util.py
+++ cbamf/util.py
@@ -203,6 +203,24 @@ class Tile(object):
         rr = np.abs(amax(self.r - tile.r, 0))
         return ll, rr
 
+    def reflect_overhang(self, clip):
+        """
+        Compute the overhang and reflect it internally so respect periodic
+        padding rules (see states._tile_from_particle_change). Returns both
+        the inner tile and the inner tile with necessary pad.
+        """
+        orig = self.copy()
+        tile = self.copy()
+
+        hangl, hangr = tile.overhang(clip)
+        tile = tile.pad(hangl)
+        tile = tile.pad(hangr)
+
+        inner = Tile.intersection([clip, orig])
+        outer = Tile.intersection([clip, tile])
+        return inner, outer
+
+
 class RawImage():
     def __init__(self, filename, tile=None, zstart=None, zstop=None,
             xysize=None, invert=False):
