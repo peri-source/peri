df3abb1914670d78ecb134a7a4de6fef1588e733
Matt Bierbaum <matt.bierbaum@gmail.com>
2016-04-21 11:57:07 -0400

    updating platonic sphere generation comparison

--- scripts/platonic-image.py
+++ scripts/platonic-image.py
@@ -9,9 +9,9 @@ from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 from mpl_toolkits.axes_grid1 import ImageGrid
 
 import common
-from cbamf.viz import util
+from cbamf.viz import base
 from cbamf.util import Tile
-from cbamf.comp.objs import exact_volume_sphere, sphere_analytical_gaussian 
+from cbamf.comp.objs import exact_volume_sphere, sphere_analytical_gaussian_fast
 
 def calc(N=28, scale=41):
     pos = np.array([(N-1)/2.0]*3)
@@ -21,8 +21,9 @@ def calc(N=28, scale=41):
     im,pos,pix = common.perfect_platonic_per_pixel(N, 5, scale=scale, returnpix=corner)
 
     tile = Tile(im.shape)
-    rvec = np.sqrt(((tile.coords(form='vector') - pos)**2).sum(axis=-1))
-    approx = exact_volume_sphere(rvec, radius, function=sphere_analytical_gaussian, volume_error=1e-10)
+    rvec = tile.coords(form='vector')
+    approx = exact_volume_sphere(rvec, pos, radius, 1.0,
+            function=sphere_analytical_gaussian_fast, volume_error=1e-10)
 
     return im, pix, approx, corner
 
@@ -35,14 +36,15 @@ def go2():
 
 def showpic(im, ax, title='', cmap='bone', c=0.5, d=0.5):
     size = [0, im.shape[0], 0, im.shape[1]]
-    ax.imshow(im, extent=size, cmap=cmap, vmin=c-d, vmax=c+d)
+    image = ax.imshow(im, extent=size, cmap=cmap, vmin=c-d, vmax=c+d)
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_xlim(0, im.shape[0])
     ax.set_ylim(0, im.shape[1])
     ax.set_title(title, fontsize=28)
+    return image
 
-def doplot1(im, pix, corner):
+def doplot1(im, pix, corner, *args):
     fig = pl.figure(figsize=(8,8))
     ax = fig.add_axes([0,0,1,1])
     im_s = im[corner[0]]
@@ -57,18 +59,20 @@ def doplot1(im, pix, corner):
     mark_inset(ax, zoom, loc1=2, loc2=4, fc="none", ec="1.0")
 
 def doplot3(im, pix, approx, corner):
-    fig = pl.figure(figsize=(12,5))
-    gs = ImageGrid(fig, rect=[0.025, 0.025, 0.95, 0.95], nrows_ncols=(1,3),
+    fig = pl.figure(figsize=(14,5))
+    gs = ImageGrid(fig, rect=[0.025, 0.025, 0.88, 0.95], nrows_ncols=(1,3),
             axes_pad=0.1)
+    cbax = fig.add_axes([0.905, 0.095, 0.0125, 0.81])
 
     im_superres = im[corner[0]]
     im_superpix = pix[pix.shape[0]/2]
     im_approx = approx[corner[0]]
 
     extent = [0,im_superres.shape[0], 0, im_superres.shape[1]]
-    showpic(im_superres, gs[0], title='Super-resolution')
-    showpic(im_approx, gs[1], title='Approximation')
-    showpic(im_superres-im_approx, gs[2], cmap='RdBu', c=0, d=0.5, title='Difference')
+    im0 = showpic(im_superres, gs[0], title='Super-resolution')
+    im1 = showpic(im_approx, gs[1], title='Approximation')
+    im2 = showpic(im_superres-im_approx, gs[2], cmap='RdBu', c=0, d=0.1, title='Difference')
+    pl.colorbar(im2, cbax)
 
     extent2 = [corner[1], corner[1]+1, corner[2], corner[2]+1]
     zoom = zoomed_inset_axes(gs[0], 8, loc=3)
