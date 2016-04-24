afa23c4dd59a36f42166029043c6f55013525d04
Matt Bierbaum <matt.bierbaum@gmail.com>
2016-04-24 15:16:03 -0400

    difference image function; switching order and labels

--- scripts/figures/model-complexity-tables.py
+++ scripts/figures/model-complexity-tables.py
@@ -1,8 +1,10 @@
 import copy
 import numpy as np
 import scipy as sp
-import pylab as pl
 from collections import OrderedDict
+
+import matplotlib.pyplot as pl
+from mpl_toolkits.axes_grid1 import ImageGrid
 
 from cbamf import util, runner
 from cbamf.test import nbody
@@ -22,7 +24,7 @@ def create_image(N=128, size=64, radius=6.0, pad=16):
 
     slab_zpos = -radius
     s = runner.create_state(
-        blank, pos, rad, slab=slab_zpos, sigma=0.001,
+        blank, pos, rad, slab=slab_zpos, sigma=0.0001,
         stateargs={'pad': pad, 'offset': 0.18},
         psftype='cheb-linescan', psfargs={'zslab': slab_zpos, 'cheb_degree': 6, 'cheb_evals': 8},
         ilmtype='barnesleg2p1dx', ilmargs={'order': (1,1,3), 'npts': (30,10,5)}
@@ -54,7 +56,7 @@ def table(s, datas, names, vary_func):
     model_image = s.image[s.inner][slicer].copy()
 
     results = OrderedDict()
-    results['reference'] = (0, model_image, p0, r0)
+    results['Reference'] = (model_image, p0, r0)
 
     for i, (name, data) in enumerate(zip(names, datas)):
         print i, name, data
@@ -68,7 +70,7 @@ def table(s, datas, names, vary_func):
         optimize(state)
 
         results[name] = (
-            i, state.get_difference_image()[slicer].copy(),
+            state.get_difference_image()[slicer].copy(),
             state.obj.pos.copy(),
             state.obj.rad.copy()
         )
@@ -86,10 +88,10 @@ def table_platonic():
         ('exact-gaussian-fast',)
     ]
     names = [
-        'Boolean cut',
-        'Linear interpolation',
-        'Logistic function',
-        'Exact Gaussian convolution'
+        r'Boolean cut',
+        r'Linear interpolation',
+        r'Logistic function',
+        r'Exact Gaussian convolution'
     ]
 
     def vary_func(s, data):
@@ -111,11 +113,11 @@ def table_ilms():
         ilms.BarnesStreakLegPoly2P1DX3(shape=s.ilm.shape, order=s.ilm.order, npts=(30,10,5)),
     ]
     names = [
-        'Legendre 2+1D (0,0,0)',
-        'Legendre 2+1D (2,2,2)',
-        'Barnes streaks (10, 5), 0th order z',
-        'Barnes streaks (30, 10), 1st order z',
-        'Barnes streaks (30, 10, 5), 2nd order z',
+        r'Legendre 2+1D (0,0,0)',
+        r'Legendre 2+1D (2,2,2)',
+        r'Barnes (10, 5), $N_z=1$',
+        r'Barnes (30, 10), $N_z=2$',
+        r'Barnes (30, 10, 5), $N_z=3$',
     ]
 
     def vary_func(s, data):
@@ -135,11 +137,11 @@ def table_psfs():
         exactpsf.ChebyshevLineScanConfocalPSF(shape=s.psf.shape, zrange=(0, s.psf.shape[0]), cheb_degree=6, cheb_evals=8),
     ]
     names = [
-        'Identity',
-        'Gaussian(x,y)',
-        'Gaussian(x,y,z,z\')',
-        'Chebyshev line scan (3,6)',
-        'Chebyshev line scan (6,8)',
+        r'Identity',
+        r'Gaussian(x,y)',
+        r'Gaussian(x,y,z,z\')',
+        r'Cheby linescan (3,6)',
+        r'Cheby linescan (6,8)',
     ]
 
     def vary_func(s, data):
@@ -154,26 +156,26 @@ def gogogo():
     return r0, r1, r2
 
 # temporary helper function
-def toOD(table):
-    return OrderedDict(sorted(table.iteritems(), key=lambda x: x[1][0]))
+#def toOD(table):
+#    return OrderedDict(sorted(table.iteritems(), key=lambda x: x[1][0]))
 
 def scores(results):
     tmp = copy.copy(results)
 
     scores = []
     for result in tmp:
-        ref = result.pop('reference')
+        ref = result.pop('Reference')
 
         errors = OrderedDict()
         for k,v in result.iteritems():
             errors[k] = (
-                np.sqrt(((ref[2] - v[2])**2).sum(axis=-1)).mean(),
-                np.sqrt(((ref[2] - v[2])**2).sum(axis=-1)).std(),
-                np.sqrt((ref[3] - v[3])**2).mean(),
-                np.sqrt((ref[3] - v[3])**2).std(),
+                np.sqrt(((ref[1] - v[1])**2).sum(axis=-1)).mean(),
+                np.sqrt(((ref[1] - v[1])**2).sum(axis=-1)).std(),
+                np.sqrt((ref[2] - v[2])**2).mean(),
+                np.sqrt((ref[2] - v[2])**2).std(),
             )
 
-        result['reference'] = ref
+        result['Reference'] = ref
         scores.append(errors)
     return scores
 
@@ -189,5 +191,28 @@ def print_table(tbl):
 def make_plots(results):
     ln = len(results)
 
-    for k,v in results.iteritems():
-        pass
+    size = 3
+    fig = pl.figure(figsize=(ln*size, size+2.0/ln))
+    img = ImageGrid(
+        fig, rect=[0.05/ln, 0.05, 1-0.1/ln, 0.90],
+        nrows_ncols=[1, ln], axes_pad=0.1
+    )
+
+    mins, maxs = [], []
+    for i, (k,v) in enumerate(results.iteritems()):
+        if k == 'Reference':
+            continue
+        mins.append(v[1].min())
+        maxs.append(v[1].max())
+    mins = min(mins)
+    maxs = max(maxs)
+
+    for i, (k,v) in enumerate(results.iteritems()):
+        if k == 'Reference':
+            img[i].imshow(v[1], vmin=0, vmax=1, cmap='bone')
+        else:
+            img[i].imshow(v[1], vmin=mins, vmax=maxs)
+
+        img[i].set_title(k.capitalize(), fontsize=12)
+        img[i].set_xticks([])
+        img[i].set_yticks([])
