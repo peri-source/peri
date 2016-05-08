88eb64586b5d584d5b719f2641c92f76997ecc9e
Brian Leahy <bdl48@cornell.edu>
2016-05-08 13:11:48 -0400

    addsub.add_subtract

--- cbamf/opt/addsubtract.py
+++ cbamf/opt/addsubtract.py
@@ -3,7 +3,7 @@ from cbamf import initializers
 import trackpy as tp
 import cbamf.opt.optimize as opt
 
-def feature_guess(st, rad, invert=True, minmass=None, use_tp=False):
+def feature_guess(st, rad, invert=True, minmass=None, use_tp=False, **kwargs):
     if minmass == None:
         #30% of the feature size mass is a good cutoff empirically for
         #initializers.local_max_featuring, less for trackpy; 
@@ -34,8 +34,8 @@ def feature_guess(st, rad, invert=True, minmass=None, use_tp=False):
     guess += st.pad
     return guess[inds].copy(), npart
 
-def check_add_particles(st, guess, rad='calc', im_change_frac=0.2, 
-        do_opt=True, opt_box_scale=2.5, quiet=True):
+def check_add_particles(st, guess, rad='calc', do_opt=True, opt_box_scale=2.5,
+        quiet=True, **kwargs):
     """
     comments
     st = state
@@ -49,58 +49,68 @@ def check_add_particles(st, guess, rad='calc', im_change_frac=0.2,
     new_inds = []
     if rad == 'calc':
         rad = np.median(st.obj.rad[st.obj.typ==1])
+    if not quiet:
+        print '-'*30 + 'ADDING' + '-'*30
+        print '  R\t  Z\t  Y\t  X\t|\t ERR0\t\t ERR1'
     for a in xrange(guess.shape[0]):
         p = guess[a]
         if not quiet:
             old_err = opt.get_err(st)
-        n = st.add_particle(p, rad)
+        ind = st.add_particle(p, rad)
         if do_opt:
             # db = opt_box_scale * np.ones(p.shape) * rad
             # bounds = [(p-db).tolist(), (p+db).tolist()]
             # inds = opt.find_particles_in_box(st, bounds)
-            lp = opt.LMParticles(st, np.array([n],dtype='int'), damping=1.0, 
-                    max_iter=1, run_length=3, particle_kwargs={'include_rad':False})
+            lp = opt.LMParticles(st, np.array([ind],dtype='int'), damping=1.0, 
+                    max_iter=2, run_length=3, particle_kwargs={'include_rad':False})
             lp.do_run_2()
-            # opt.do_levmarq_particles(st, np.array([n],dtype='int'), damp=1.0, 
+            # opt.do_levmarq_particles(st, np.array([ind],dtype='int'), damp=1.0, 
                     # num_iter=1, run_length=3, quiet=True, include_rad=False)
             # opt.do_levmarq_particles(st, inds, damp=1.0, 
                     # num_iter=1, run_length=3, quiet=True)
             #ok... but still doesn't get the clusters well
-        did_kill = check_remove_particle(st, n, im_change_frac=im_change_frac)
+        did_kill = check_remove_particle(st, ind, **kwargs)
         if not did_kill:
             accepts += 1
-            new_inds.append(n)
+            new_inds.append(ind)
             if not quiet:
-                print '%d:\t%f\t%f' % (a, old_err, opt.get_err(st))
+                # print '%d:\t%f\t%f' % (a, old_err, opt.get_err(st))
+                print '%2.2f\t%3.2f\t%3.2f\t%3.2f\t|\t%4.3f\t\t%4.3f' % (
+                        st.obj.rad[ind], st.obj.pos[ind,0], st.obj.pos[ind,1], 
+                        st.obj.pos[ind,2], old_err, opt.get_err(st))
     return accepts, new_inds
     
-def check_remove_particle(st, n, im_change_frac=0.2):
+def check_remove_particle(st, n, im_change_frac=0.2, min_derr='3sig', **kwargs):
+    """
+    Checks whether to remove particle 'n' from state 'st'. If removing the
+    particle increases the error by less than max( min_derr, change in image *
+            im_change_frac), then the particle is removed. 
+    """
+    if min_derr == '3sig':
+        min_derr = 3 * st.sigma
     present_err = opt.get_err(st); present_d = st.get_difference_image().copy()
     p, r = st.remove_particle(n)
     absent_err = opt.get_err(st); absent_d = st.get_difference_image().copy()
     
     im_change = np.sum((present_d - absent_d)**2)
-    if (absent_err - present_err) >= im_change_frac * im_change:
-        #If present err is sufficiently better than absent, add it back
-        #if present_err - absent_err is sufficiently negative, add it back
-        #if absent_err - present_err is sufficiently positive, add it back
+    if (absent_err - present_err) >= max([im_change_frac * im_change, min_derr]):
         st.add_particle(p, r)
         killed = False
     else:
         killed = True
     return killed
 
-def sample_n_add(st, rad='calc', tries=20, quiet=True, do_opt=True, 
-        im_change_frac=0.2, opt_box_scale=3, **kwargs):
+def sample_n_add(st, rad='calc', tries=20, **kwargs):
+    """
+    quiet=True, do_opt=True, im_change_frac=0.2, opt_box_scale=3,
+    """
     if rad == 'calc':
         rad = np.median(st.obj.rad[st.obj.typ==1])
     
     guess, npart = feature_guess(st, rad, **kwargs)
     tries = np.min([tries, npart])
     
-    accepts, new_inds = check_add_particles(st, guess[:tries], rad=rad, 
-            im_change_frac=0.2, do_opt=do_opt, opt_box_scale=opt_box_scale, 
-            quiet=quiet)
+    accepts, new_inds = check_add_particles(st, guess[:tries], rad=rad, **kwargs)
     return accepts, new_inds
 
 def remove_bad_particles(s, min_rad=2.0, max_rad=12.0, min_edge_dist=2.0, 
@@ -179,16 +189,17 @@ def remove_bad_particles(s, min_rad=2.0, max_rad=12.0, min_edge_dist=2.0,
     rad_wrong_size = np.nonzero(((s.obj.rad < min_rad) | 
             (s.obj.rad > max_rad)) & typ)[0]
     near_im_edge = np.nonzero(is_near_im_edge(s.obj.pos, min_edge_dist) & typ)[0]
-    delete_inds = np.unique(np.append(rad_wrong_size, near_im_edge))
+    delete_inds = np.unique(np.append(rad_wrong_size, near_im_edge)).tolist()
     if not quiet:
-        print '  R\t  Z\t  Y\t  X\t|\t  ERR0\t\t  ERR1'
+        print '-'*27 + 'SUBTRACTING' + '-'*28
+        print '  R\t  Z\t  Y\t  X\t|\t ERR0\t\t ERR1'
 
     for ind in delete_inds:
         er0 = opt.get_err(s)
         p, r = s.remove_particle(ind)
         er1 = opt.get_err(s)
         if not quiet:
-            print '%2.2f\t%3.2f\t%3.2f\t%3.2f\t|\t%5.3f\t%5.3f' % (
+            print '%2.2f\t%3.2f\t%3.2f\t%3.2f\t|\t%5.3f\t\t%5.3f' % (
                     s.obj.rad[ind], s.obj.pos[ind,0], s.obj.pos[ind,1], 
                     s.obj.pos[ind,2], er0, er1)
         removed += 1
@@ -213,9 +224,111 @@ def remove_bad_particles(s, min_rad=2.0, max_rad=12.0, min_edge_dist=2.0,
         killed = check_remove_particle(s, ind, im_change_frac=im_change_frac)
         if killed:
             removed += 1
+            delete_inds.append(ind)
             er1 = opt.get_err(s)
             if not quiet:
                 print '%2.2f\t%3.2f\t%3.2f\t%3.2f\t|\t%5.3f\t%5.3f' % (
                         s.obj.rad[ind], s.obj.pos[ind,0], s.obj.pos[ind,1], 
                         s.obj.pos[ind,2], er0, er1)
-    return removed
+    return removed, delete_inds
+
+def add_subtract(st, max_iter=5, **kwargs):
+    """
+    Adds and subtracts particles. 
+    
+    Parameters
+    ----------
+        st: ConfocalImagePython
+            The state to add and subtract particle too. 
+        max_iter : Int
+            The maximum number of add-subtract loops to use. Default is 5.
+            Terminates after either max_iter loops or when nothing has
+            changed. 
+        
+    **kwargs Parameters
+    -------------------
+        quiet : Bool
+            Set to False to print out information about add/subtract
+            particles and change in fit error. 
+        invert : Bool
+            True if the particles are dark on a bright background, False
+            if they are bright on a dark background. Default is True. 
+        min_rad : Float
+            Particles with radius below min_rad are automatically deleted.
+            Default is 2.0.
+        max_rad : Float
+            Particles with radius below min_rad are automatically deleted.
+            Default is 12.0, but you should change this for your partilce
+            sizes. 
+        min_edge_dist : Float
+            Particles closer to the edge of the padded image than this
+            are automatically deleted. Default is 2.0. 
+        check_rad_cutoff : 2-element float list. 
+            Particles with radii < check_rad_cutoff[0] or > check...[1]
+            are checked if they should be deleted (not automatic). 
+            Default is [3.5, 15].
+        check_outside_im : Bool
+            Set to True to check whether to delete particles whose 
+            positions are outside the un-padded image.
+            
+        rad : Float
+            The initial radius for added particles; added particles radii
+            are not fit until the end of add_subtract. Default is 'calc',
+            which uses the median radii of active particles. 
+
+        tries : 
+        im_change_frac
+        min_derr : Float
+            The minimum Default is '3sig' which uses 3*st.sigma. 
+        
+        do_opt : Bool
+            Set to False to avoid optimizing particle positions after
+            adding them. 
+        minmass : Float
+            
+        use_tp : Bool
+            Set to True to use trackpy to find missing particles inside
+            the image. Not recommended since it trackpy deliberately
+            cuts out particles at the edge of the image. Default is False.
+        
+    
+    Outputs
+    -------
+        total_changed : Int. 
+            The total number of adds and subtracts done on the data. 
+            Not the same as changed_inds.size since the same particle
+            or particle index can be added/subtracted multiple times.
+        changed_inds : np.ndarray of ints
+            The particle indices that were added and/or subtracted during
+            the fit. 
+    
+    Comments
+    --------
+        Occasionally after the intial featuring a cluster of particles
+        is featured as 1 big particle. To fix these mistakes, it helps
+        to set max_rad to a physical value. This removes the big particle
+        and allows it to be re-featured by (several passes of) the adding
+        portion. 
+        
+    To implement
+    ------------
+        A way to check regions that are poorly fit (e.g. skew- or kurtosis-
+        hunt).
+    """
+    total_changed = 0
+    added_inds = []
+    removed_inds = []
+    
+    for _ in xrange(max_iter):
+        nr, rinds = remove_bad_particles(st, **kwargs)
+        na, ainds = sample_n_add(st, **kwargs)
+        current_changed = na + nr
+        added_inds.extend(ainds)
+        removed_inds.extend(rinds)
+        total_changed += current_changed
+        if current_changed == 0:
+            break
+    #Now we optimize the radii too:
+    for i in added_inds:
+        opt.do_levmarq_particles(st, np.array([i]), max_iter=2, damping=0.3)
+    return total_changed, np.unique(added_inds + removed_inds)
