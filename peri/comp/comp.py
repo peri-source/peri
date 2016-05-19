f238a9301814f85219b8efa689d68bf2373d016c
Matt Bierbaum <matt.bierbaum@gmail.com>
2016-05-19 12:14:54 -0400

    added ParameterGroup, Component abstractions

--- peri/comp/comp.py
+++ peri/comp/comp.py
@@ -1,72 +1,88 @@
-# TODO -- register parameters with a function that gets a unique number
-# probably a counter.  then, building the parameters would look like:
-#
-#     pgrp = ['b1', 'b2', 'zscale', ...]
-#     pnum = [4, 5, 1, ...]
-#
-# creates:
-#
-#     pdict = {'b1_0': 0, 'b1_1': 1, ..., 'zscale': 9, ...}
-#     blocks = pdict.keys()
+from collections import OrderedDict
 
 class ParameterGroup(object):
     category = 'param'
 
-    def __init__(self, params):
-        self.p2i = {}
-        self.params = np.array(params)
-
-    def _setup_param_dict(self):
-        for i, param in enumerate(self.params):
-            self.p2i[self.category+'-'+i] = i
-
-    def _update_values(self, params, values):
-        for param, value in zip(params, values):
-            self.params[self.p2i[param]] = value
-
-    def initialize(self):
-        pass
+    def __init__(self, params=None, values=None):
+        """
+        Set up a parameter group, which is essentially an OrderedDict of param
+        -> values. However, it is generalized so that this structure is not
+        strictly enforced for all ParameterGroup subclasses.
+        """
+        if params is not None and values is not None:
+            self._params = OrderedDict()
+            for p,v in zip(params, values):
+                self._params[p] = v
+        elif params is not None and values is None:
+            self._params = OrderedDict()
+            for p in params:
+                self._params[p] = 0
+        else:
+            self._params = OrderedDict()
 
     def update(self, params, values):
-        self._update_values(params, values)
+        """
+        Update the a single (param, value) combination, or a list or tuple of
+        params and corresponding values for the object.
+        """
+        self.set_param(params, values)
 
-    def get_param(self, param):
-        return self.params[self.p2i[param]]
+    def get_values(self, params):
+        """ Get the value of a list or single parameter """
+        if isinstance(params, (tuple, list)):
+            return [self._params[p] for p in params]
+        return self._params[params]
 
-    def get_param_vector(self):
-        return self.params
+    def set_values(self, params, values):
+        """
+        Directly set a single (param, value) combination, or a list or tuple of
+        params and corresponding values for the object.
+        """
+        if isinstance(params, (tuple, list)):
+            for p, v in zip(params, values):
+                self._params[p] = v
+        self._params[params] = values
 
-    def get_param_names(self):
-        pass
+    @propery
+    def params(self):
+        return self._params.keys()
 
-    def get_param_categories(self):
-        pass
+    @property
+    def values(self):
+        return self._params.values()
+
 
 class Component(ParameterGroup):
     # TODO make all components serializable via _getinitargs_
-    def __init__(self, params, shape):
+    def __init__(self, params):
         pass
 
-    def set_tile(self, tile):
-        pass
-
-    def get_support_size(self, blocks):
+    def get_support_size(self, params, values):
         """
-        This method returns the actual image area to be modified by
-        the update to `blocks'. 
+        This method returns a `peri.util.Tile` object defining the region of
+        a field that has to be modified by the update of (params, values).
 
         Parameters:
         -----------
-        blocks: list-like, Block object
-            an array 
-        """
-        pass
+        params : single param, list of params
+            A single parameter or list of parameters to be updated
 
-    def get_pad_size(self):
-        pass
+        values : single value, list of values
+            The values corresponding to the params
+
+        Returns:
+        --------
+        tile : `peri.util.Tile`
+            A tile corresponding to the image region
+        """
+        raise NotImplementedError("get_support_size required for components")
+
+    def get_padding_size(self, params, values):
+        raise NotImplementedError("get_padding_size required for components")
 
     def get_field(self):
-        pass
+        raise NotImplementedError("get_field required for components")
 
-class Prior(ParameterGroup):
-    pass
+    def set_tile(self, tile):
+        raise NotImplementedError("set_tile required for components")
+
