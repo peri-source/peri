d4b36855c5219d0587fcaa70a833b786af81ff67
Matt Bierbaum <matt.bierbaum@gmail.com>
2016-05-20 14:53:05 -0400

    monkey-patch the trackpy logging so that it doesn't interfere
    Conflicts:
    	cbamf/__init__.py

--- peri/__init__.py
+++ peri/__init__.py
@@ -1,2 +1,10 @@
 # always monkey-patch the matplotlibrc for peri!
 from peri.viz import base
+
+# trackpy loggers interfere with our own. stop that here.
+try:
+    import trackpy
+    import logging
+    logging.root.removeHandler(logging.root.handlers[0])
+except ImportError as e:
+    pass
