d125b371c0226812481893001e0bd24ec282e1e1
Matt Bierbaum <matt.bierbaum@gmail.com>
2016-06-17 16:55:44 -0400

    making requirements files

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 #from setuptools import setup
 from distutils.core import setup
 
+reqs = open('./requirements.txt').readlines()
+
 setup(name='peri',
       license='MIT License',
-      author='Matt Bierbaum, Brian Leahy, Alex Alemi',
+      author='Matt Bierbaum, Brian Leahy',
       version='0.1.1',
 
-      packages=['peri', 'peri.mc', 'peri.comp', 'peri.viz', 'peri.priors', 'peri.test', 'peri.opt'],
-      install_requires=[
-          "numpy>=1.8.1",
-          "scipy>=0.14.0",
-          "matplotlib>=1.0.0",
-          "pyfftw>=0.9.1",
-          "pillow>=1.1.7"
+      packages=[
+          'peri', 'peri.mc', 'peri.comp', 'peri.viz',
+          'peri.priors', 'peri.test', 'peri.opt'
       ],
+      install_requires=reqs,
       scripts=['bin/peri']
 )
