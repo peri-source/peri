6be1a2e181b6433c787ebba05009fdd19cb30aca
Brian Leahy <bdl48@cornell.edu>
2016-09-21 10:45:24 -0400

    opt: Adding vectorize_damping function.

--- peri/opt/optimize.py
+++ peri/opt/optimize.py
@@ -112,6 +112,32 @@ def get_num_px_jtj(s, nparams, decimate=1, max_mem=1e9, min_redundant=20, **kwar
     num_px = np.clip(px_dec, px_red, px_mem)
     return num_px
 
+def vectorize_damping(params, damping=1.0, increase_list=[['psf-', 1e4]]):
+    """
+    Returns a non-constant damping vector, allowing certain parameters to be
+    more strongly damped than others.
+    Parameters
+    ----------
+        params : List
+            The list of parameter names, in order.
+        damping : Float
+            The default value of the damping.
+        increase_list: List
+            A nested 2-element list of the params to increase and their
+            scale factors. All parameters containing the string
+            increase_list[i][0] are increased by a factor increase_list[i][1].
+    Returns
+    -------
+        damp_vec : np.ndarray
+            The damping vector to use.
+    """
+    damp_vec = np.ones(len(params)) * damping
+    for nm, fctr in increase_list:
+        for a in xrange(damp_vec.size):
+            if nm in params[a]:
+                damp_vec[a] *= fctr
+    return damp_vec
+
 #=============================================================================#
 #               ~~~~~  Particle Optimization stuff  ~~~~~
 #=============================================================================#
