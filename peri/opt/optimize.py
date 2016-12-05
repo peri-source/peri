0047a8b31eaecae37d681253fb14aa1bb5a394c8
Brian Leahy <bdl48@cornell.edu>
2016-12-05 16:50:43 -0500

    LMEngine: ~100+x faster calc_model_cosine.

--- peri/opt/optimize.py
+++ peri/opt/optimize.py
@@ -1128,28 +1128,60 @@ class LMEngine(object):
                 np.dot(np.dot(self.JTJ, delta_params), delta_params))
         return expected_error
 
-    def calc_model_cosine(self, decimate=None):
+    def calc_model_cosine(self, decimate=None, mode='err'):
         """
-        Calculates the cosine of the fittable residuals with the actual
-        residuals, cos(phi) = |P^T r| / |r| where P^T is the projection
-        operator onto the model manifold and r the residuals.
+        Calculates the cosine of the residuals with the model.
 
-        `Decimate' allows for every nth pixel only to be counted for speed.
-        While this is n x faster, it is considerably less accurate, so the
-        default is no decimation. (set decimate to an int or None).
+        Parameters
+        ----------
+            decimate : Int or None, optional
+                Decimate the residuals by `decimate` pixels. If None, no
+                decimation is used. Valid only with mode='svd'. Default
+                is None
+            mode : {'svd', 'err'}
+                Which mode to use; see Notes section. Default is 'err'.
+
+        Returns
+        -------
+            abs_cos : numpy.float64
+                The absolute value of the model cosine.
+
+        Notes
+        -----
+        The model cosine is defined in terms of the geometric view of
+        curve-fitting, as a model manifold embedded in a high-dimensional
+        space. The model cosine is the cosine of the residuals vector
+        with its projection on the tangent space: cos(phi) = |P^T r|/|r|
+        where P^T is the projection operator onto the model manifold and r
+        the residuals. This can be calculated two ways: By calculating
+        the projection operator P directly with SVD (mode=`svd`), or by
+        using the expected error if the model were linear to calculate a
+        model sine first (mode=`err`). Since the SVD of a large matrix is
+        slow, mode=`err` is faster.
+
+        `Decimate' allows for every nth pixel only to be counted in the
+        SVD matrix of J for speed. While this is n x faster, it is
+        considerably less accurate, so the default is no decimation.
         """
-        slicer = slice(0,-1,decimate)
+        if mode == 'svd':
+            slicer = slice(0,-1,decimate)
 
-        #1. Calculate projection term
-        u, sig, v = np.linalg.svd(self.J[:,slicer], full_matrices=False) #slow part
-        # p = np.dot(v.T, v) - memory error, so term-by-term
-        r = self.calc_residuals()[slicer]
-        abs_r = np.sqrt((r*r).sum())
+            #1. Calculate projection term
+            u, sig, v = np.linalg.svd(self.J[:,slicer], full_matrices=False) #slow part
+            # p = np.dot(v.T, v) - memory error, so term-by-term
+            r = self.calc_residuals()[slicer]
+            abs_r = np.sqrt((r*r).sum())
 
-        v_r = np.dot(v,r/abs_r)
-        projected = np.dot(v.T, v_r)
+            v_r = np.dot(v,r/abs_r)
+            projected = np.dot(v.T, v_r)
 
-        abs_cos = np.sqrt((projected*projected).sum())
+            abs_cos = np.sqrt((projected*projected).sum())
+        elif mode == 'err':
+            expected_error = self.find_expected_error(delta_params='perfect')
+            model_sine_2 = expected_error / self.error  #error = distance^2
+            abs_cos = np.sqrt(1 - model_sine_2)
+        else:
+            raise ValueError('mode must be one of `svd`, `err`')
         return abs_cos
 
     def get_termination_stats(self, get_cos=True):
