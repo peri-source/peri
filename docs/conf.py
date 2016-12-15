e969c6b10c1876e37b9d8b48167a9e215e1ca321
Brian Leahy <bdl48@cornell.edu>
2016-12-15 16:56:07 -0500

    Updating conf file to only include 1 mathjax.

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ alabaster = None
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.pngmath',
+    'sphinx.ext.autodoc', 'sphinx.ext.todo',
     'sphinx.ext.mathjax', 'sphinx.ext.viewcode', 'sphinxcontrib.napoleon'
 ]
 autodoc_member_order = 'bysource'
