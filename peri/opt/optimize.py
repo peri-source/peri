6c7b6cbde3e88d7ab40955ee8fa76bf46c600413
Brian Leahy <bdl48@cornell.edu>
2016-12-16 17:19:04 -0500

    LMGlobals.find_expected_error, calc_model_cosine now distinguish between full and decimated errors.

--- peri/opt/optimize.py
+++ peri/opt/optimize.py
@@ -56,7 +56,7 @@ To solve eq. (1), we need to:
 My only change to this is, instead of calculating J_ia, we calculate
 J_ia for a small subset (say 1%) of the pixels in the image randomly selected,
 rather than for all the pixels (in addition to leastsq solution instead of
-linalg.solve
+linalg.solve)
 """
 
 def get_rand_Japprox(s, params, num_inds=1000, **kwargs):
@@ -1748,6 +1748,83 @@ class LMGlobals(LMEngine):
         self.JTJ = np.dot(self.J, self.J.T)
         if np.any(np.isnan(self.J)) or np.any(np.isnan(self.JTJ)):
             raise FloatingPointError('J, JTJ have nans.')
+
+    def find_expected_error(self, delta_params='calc', adjust=True):
+        """
+        Returns the error expected after an update if the model were linear.
+
+        Parameters
+        ----------
+            delta_params : {numpy.ndarray, 'calc', or 'perfect'}, optional
+                The relative change in parameters. If 'calc', uses update
+                calculated from the current damping, J, etc; if 'perfect',
+                uses the update calculated with zero damping.
+
+        Returns
+        -------
+            numpy.float64
+                The expected error after the update with `delta_params`
+        """
+        expected_error = super(self.__class__, self).find_expected_error(
+                delta_params=delta_params)
+        if adjust:
+            #adjust for num_pix
+            derr = (expected_error - self.error) * (self.state.residuals.size /
+                    float(self.num_pix))
+            expected_error = self.error + derr
+        return expected_error
+
+    def calc_model_cosine(self, decimate=None, mode='err'):
+        """
+        Calculates the cosine of the residuals with the model.
+
+        Parameters
+        ----------
+            decimate : Int or None, optional
+                Decimate the residuals by `decimate` pixels. If None, no
+                decimation is used. Valid only with mode='svd'. Default
+                is None
+            mode : {'svd', 'err'}
+                Which mode to use; see Notes section. Default is 'err'.
+
+        Returns
+        -------
+            abs_cos : numpy.float64
+                The absolute value of the model cosine.
+
+        Notes
+        -----
+        The model cosine is defined in terms of the geometric view of
+        curve-fitting, as a model manifold embedded in a high-dimensional
+        space. The model cosine is the cosine of the residuals vector
+        with its projection on the tangent space: :math:`cos(phi) = |P^T r|/|r|`
+        where :math:`P^T` is the projection operator onto the model manifold
+        and :math:`r` the residuals. This can be calculated two ways: By
+        calculating the projection operator P directly with SVD (mode=`svd`),
+        or by using the expected error if the model were linear to calculate
+        a model sine first (mode=`err`). Since the SVD of a large matrix is
+        slow, mode=`err` is faster.
+
+        `decimate` allows for every nth pixel only to be counted in the
+        SVD matrix of J for speed. While this is n x faster, it is
+        considerably less accurate, so the default is no decimation.
+        """
+        #we calculate the model cosine only in the data space of the
+        #sampled indices
+        if mode == 'err':
+            expected_error = self.find_expected_error(delta_params='perfect',
+                    adjust=False)
+            derr = self.error - expected_error
+            residuals_err = lambda r: np.dot(r,r).sum()
+            current_partial_error = residuals_err(self.calc_residuals())
+            expected_partial_error = current_partial_error - derr
+            model_sine_2 = expected_partial_error / current_partial_error
+            abs_cos = np.sqrt(1 - model_sine_2)
+        else:
+            #superclass is fine
+            abs_cos = super(self.__class__, self).calc_model_cosine(decimate=
+                    decimate, mode=mode)
+        return abs_cos
 
 class LMParticles(LMEngine):
     """
