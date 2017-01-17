4407ba403411d5600044db621e8b03c83716393a
Brian Leahy <bdl48@cornell.edu>
2017-01-17 16:19:24 -0500

    adding runner.finish_state

--- peri/runner.py
+++ peri/runner.py
@@ -632,3 +632,33 @@ def link_zscale(st):
     psf.param_dict.pop('psf-zscale')
     st.trigger_parameter_change()
     st.reset()
+
+def finish_state(st, desc='finish-state'):
+    """
+    Final optimization for the perfectionist.
+
+    Runs a local add-subtract to capture any difficult-to-feature particles,
+    then does another set of optimization designed to get to the best
+    possible fit.
+
+    Parameters
+    ----------
+        st : :class:`peri.states.ImageState`
+            The state to finish
+        desc : String, optional
+            Description to intermittently save the state as, as passed to
+            state.save. Default is `'finish-state'`.
+
+    See Also
+    --------
+        `peri.opt.addsubtract.add_subtract_locally`
+        `peri.opt.optimize.finish`
+    """
+    #1. Missing particles
+    for _ in xrange(3):
+        npart, poses = addsub.add_subtract_locally(st, region_depth=7)
+        if npart == 0:
+            break
+    opt.finish(st, n_loop=1, separate_psf=True, desc=desc)
+    opt.burn(st, mode='polish', desc=desc, n_loop=2)
+    opt.finish(st, desc=desc, n_loop=4)
