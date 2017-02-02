43d017cf1326134ed261daae61449adf59639d2c
Brian Leahy <bdl48@cornell.edu>
2017-02-02 17:15:38 -0500

    exactpsf: changing a few FIXMEs to notes since they wont be fixed and are good notes.

--- peri/comp/exactpsf.py
+++ peri/comp/exactpsf.py
@@ -340,15 +340,15 @@ class ExactPSF(psfs.PSF):
 
     def characterize_psf(self):
         """ Get support size and drift polynomial for current set of params """
-        #FIXME there may be an issue with the support and characterization--
-        #   it might be best to do the characterization with the same support
-        #   as the calculated psf.
+        # there may be an issue with the support and characterization--
+        # it might be best to do the characterization with the same support
+        # as the calculated psf.
         l,u = max(self.zrange[0], self.param_dict['psf-zslab']), self.zrange[1]
 
         size_l, drift_l = self.measure_size_drift(l)
         size_u, drift_u = self.measure_size_drift(u)
 
-        # FIXME -- must be odd for now or have a better system for getting the center
+        # must be odd for now or have a better system for getting the center
         self.support = util.oddify(2*self.support_factor*size_u.astype('int'))
         self.drift_poly = np.polyfit([l, u], [drift_l, drift_u], 1)
 
