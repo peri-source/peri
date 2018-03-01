341e8cc721772d3e70ff516657deabc5056345ed
Brian Leahy <bdl48@cornell.edu>
2018-03-01 09:51:36 -0500

    Fixing assumption that particles have a radius, in opt.check_groups

--- peri/opt/optimize.py
+++ peri/opt/optimize.py
@@ -326,7 +326,7 @@ def _check_groups(s, groups):
         ans.extend(g)
     if np.unique(ans).size != np.size(ans):
         return False
-    elif np.unique(ans).size != np.size(s.obj_get_radii()):
+    elif np.unique(ans).size != s.obj_get_positions().shape[0]:
         return False
     else:
         return (np.arange(s.obj_get_radii().size) == np.sort(ans)).all()
