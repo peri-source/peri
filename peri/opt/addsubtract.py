1ba6e57d4ee96246ba17b926790c03d66441f3cd
Brian Leahy <bdl48@cornell.edu>
2018-03-06 15:36:28 -0500

    addsubtract.guess_invert to guess what invert should be from data.

--- peri/opt/addsubtract.py
+++ peri/opt/addsubtract.py
@@ -11,6 +11,30 @@ import peri.opt.optimize as opt
 from peri.logger import log
 CLOG = log.getChild('addsub')
 
+def guess_invert(st):
+    """Guesses whether particles are bright on a dark bkg or vice-versa
+
+    Works by checking whether the intensity at the particle centers is
+    brighter or darker than the average intensity of the image, by
+    comparing the median intensities of each.
+
+    Parameters
+    ----------
+    st : :class:`peri.states.ImageState`
+
+    Returns
+    -------
+    invert : bool
+        Whether to invert the image for featuring.
+    """
+    pos = st.obj_get_positions()
+    pxinds_ar = np.round(pos).astype('int')
+    inim = st.ishape.translate(-st.pad).contains(pxinds_ar)
+    pxinds_tuple = tuple(pxinds_ar[inim].T)
+    pxvals = st.data[pxinds_tuple]
+    invert = np.median(pxvals) < np.median(st.data)  # invert if dark particles
+    return invert
+
 
 def feature_guess(st, rad, invert=True, minmass=None, use_tp=False,
                   trim_edge=False, **kwargs):
