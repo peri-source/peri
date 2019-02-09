a61cd0ef926ed6dcab35d8adaef0a8c8b6895891
Brian Leahy <bdl48@cornell.edu>
2019-02-09 15:54:25 -0500

    Updating AnchoredText import to new matplotlib location

--- peri/viz/plots.py
+++ peri/viz/plots.py
@@ -31,12 +31,6 @@ def lbl(axis, label, size=22):
     """ Put a figure label in an axis """
     at = AnchoredText(label, loc=2, prop=dict(size=size), frameon=True)
     at.patch.set_boxstyle("round,pad=0.,rounding_size=0.0")
-    #bb = axis.get_yaxis_transform()
-    #at = AnchoredText(label,
-    #        loc=3, prop=dict(size=18), frameon=True,
-    #        bbox_to_anchor=(-0.5,1),#(-.255, 0.90),
-    #        bbox_transform=bb,#axis.transAxes
-    #    )
     axis.add_artist(at)
 
 def summary_plot(state, samples, zlayer=None, xlayer=None, truestate=None):
